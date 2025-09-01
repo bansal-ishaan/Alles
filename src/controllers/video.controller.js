091bad3c83481116a2215c1b83b8f3abd7f22d34
bansal-ishaan <ishaanbansal1412@gmail.com>
2025-09-02 03:14:24 +0530

    Fixed multiple api calls isssue

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -13,10 +13,7 @@ import { Like } from "../models/like.model.js";
 import { disLike } from "../models/dislike.model.js";
 
 
-// src/controllers/video.controller.js
 
-// A RADICALLY SIMPLIFIED VERSION FOR DEBUGGING
-// src/controllers/video.controller.js
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
